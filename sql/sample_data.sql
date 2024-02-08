1b6842c6c2eb195b5050498034d12b47be05e464
PiCiU1221 <96005164+PiCiU1221@users.noreply.github.com>
2024-02-08 19:44:50 +0100

    Refactor: New adverts table
    - Moved fields like date and price to the adverts table
    - New example select that combines the adverts table and the cars table
    - Swapped SERIAL to BIGSERIAL
    - Added unique constraints for some fields that should be unique

--- sql/sample_data.sql
+++ sql/sample_data.sql
@@ -1,6 +1,14 @@
 -- Initial data insertions and some sample data
--- Example cars won't be inserted if there are fewer than 3 users registered
 
+
+-- Users
+INSERT INTO users (username, email, password)
+VALUES ('testowanie1', 'testowanie1@test.pl', '$2a$10$Eh.pdFOLVr6j9YUUMLrnFePxOuNLWC9dMdAk5LbftKEwJzt.O7mAG'),
+       ('testowanie2', 'testowanie2@test.pl', '$2a$10$HfJsLZn.JMUubD.4ms0oze.ZlRI8T.ZrQLysWPPAKmeekPRcMJiAS'),
+       ('testowanie3', 'testowanie3@test.pl', '$2a$10$BY10WEly8ipWszzYQY0uPuSYNghf0pUfnscTGCXy/B/VhtxVZmIR6');
+
+
+-- Car attributes
 INSERT INTO car_makes (make_name)
 VALUES ('Volkswagen'),
        ('Ford'),
@@ -95,9 +103,9 @@ VALUES ('Beige'),
        ('Yellow');
 
 INSERT INTO car_fuel_types (fuel_type_name)
-VALUES ('Gasoline'),
-       ('Gasoline + CNG'),
-       ('Gasoline + LPG'),
+VALUES ('Petrol'),
+       ('Petrol + CNG'),
+       ('Petrol + LPG'),
        ('Diesel'),
        ('Electric'),
        ('Ethanol'),
@@ -119,10 +127,7 @@ VALUES (1),
        (3),
        (4),
        (5),
-       (6),
-       (7),
-       (8),
-       (9);
+       (6);
 
 INSERT INTO car_seat_counts (seat_count)
 VALUES (1),
@@ -144,136 +149,199 @@ VALUES ('Air Conditioning'),
        ('Leather Seats'),
        ('Navigation System');
 
--- Sample cars insertions with features
+
+-- Cars
 
 -- First car
-
--- Insert the first car and get its ID
-WITH inserted_cars AS (
-INSERT INTO cars (vin, seller_id, make_id, model_id, body_type_id, color_id, fuel_type_id, transmission_type_id, drivetrain_id, doors_id, seats_id, production_year, price, mileage, engine_power, engine_capacity, is_damaged, created_at, updated_at)
+WITH inserted_car_id AS (
+INSERT INTO cars (vin, make_id, model_id, body_type_id, color_id, fuel_type_id, transmission_type_id, drivetrain_id, doors_id, seats_id, production_year, mileage, engine_power, engine_capacity, is_damaged)
 VALUES
-    ('ABC12345678901234', 1, 1, 5, 8, 6, 1, 1, 2, 4, 5, 2019, 30000, 40000, 180, 2200, FALSE, CURRENT_TIMESTAMP, NULL)
+    ('ABC12345678901234', 1, 3, 8, 6, 4, 1, 2, 4, 5, 2017, 40000, 180, 2200, FALSE)
     RETURNING car_id
     ),
 
--- Insert car features
-    inserted_features AS (
-INSERT INTO car_features_junction (car_id, feature_id)
-SELECT car_id, feature_id
-FROM inserted_cars
-    CROSS JOIN (VALUES (1), (2), (3)) AS features(feature_id)
-    ),
-
--- Insert images into car_image_urls table and associate them with the car
     inserted_images AS (
 INSERT INTO car_image_urls (image_url)
 VALUES
     ('https://iili.io/Ja9nYej.jpg'),
     ('https://iili.io/Jad72se.jpg')
     RETURNING image_id
-    )
+    ),
 
-INSERT INTO car_image_urls_junction (car_id, image_id)
-SELECT ic.car_id, ii.image_id
-FROM inserted_cars ic
-         CROSS JOIN inserted_images ii
-WHERE ii.image_id IN (SELECT image_id FROM inserted_images);
+    image_junction AS (INSERT INTO car_image_urls_junction (car_id, image_id)
+SELECT inserted_car_id.car_id, inserted_images.image_id
+FROM inserted_car_id, inserted_images)
+
+INSERT INTO car_features_junction (car_id, feature_id)
+SELECT inserted_car_id.car_id, features.feature_id
+FROM inserted_car_id, (VALUES (1), (2), (3)) AS features(feature_id);
 
 
 -- Second car
--- Insert the second car and get its ID
-WITH inserted_cars AS (
-INSERT INTO cars (vin, seller_id, make_id, model_id, body_type_id, color_id, fuel_type_id, transmission_type_id, drivetrain_id, doors_id, seats_id, production_year, price, mileage, engine_power, engine_capacity, is_damaged, created_at, updated_at)
+WITH inserted_car_id AS (
+INSERT INTO cars (vin, make_id, model_id, body_type_id, color_id, fuel_type_id, transmission_type_id, drivetrain_id, doors_id, seats_id, production_year, mileage, engine_power, engine_capacity, is_damaged)
 VALUES
-    ('XYZ98765432109876', 2, 2, 9, 9, 11, 4, 1, 3, 5, 6, 2022, 40000, 20000, 250, 3000, FALSE, CURRENT_TIMESTAMP, NULL)
+    ('ABC12345678901235', 3, 7, 5, 12, 4, 2, 2, 4, 5, 2008, 105000, 105, 1896, FALSE)
     RETURNING car_id
     ),
 
--- Insert car features
-    inserted_features AS (
-INSERT INTO car_features_junction (car_id, feature_id)
-SELECT car_id, feature_id
-FROM inserted_cars
-    CROSS JOIN (VALUES (1), (2), (3)) AS features(feature_id)
-    ),
-
--- Insert images into car_image_urls table and associate them with the car
     inserted_images AS (
 INSERT INTO car_image_urls (image_url)
 VALUES
-    ('https://iili.io/Ja9nYea.jpg'),
-    ('https://iili.io/Jad72sb.jpg')
+    ('https://iili.io/Ja9nYmn.jpg'),
+    ('https://iili.io/Jad72vc.jpg')
     RETURNING image_id
-    )
+    ),
 
-INSERT INTO car_image_urls_junction (car_id, image_id)
-SELECT ic.car_id, ii.image_id
-FROM inserted_cars ic
-         CROSS JOIN inserted_images ii
-WHERE ii.image_id IN (SELECT image_id FROM inserted_images);
+    image_juction AS (INSERT INTO car_image_urls_junction (car_id, image_id)
+SELECT inserted_car_id.car_id, inserted_images.image_id
+FROM inserted_car_id, inserted_images)
+
+INSERT INTO car_features_junction (car_id, feature_id)
+SELECT inserted_car_id.car_id, features.feature_id
+FROM inserted_car_id, (VALUES (1), (2)) AS features(feature_id);
 
 
 -- Third car
--- Insert the third car and get its ID
-WITH inserted_cars AS (
-INSERT INTO cars (vin, seller_id, make_id, model_id, body_type_id, color_id, fuel_type_id, transmission_type_id, drivetrain_id, doors_id, seats_id, production_year, price, mileage, engine_power, engine_capacity, is_damaged, created_at, updated_at)
+WITH inserted_car_id AS (
+INSERT INTO cars (vin, make_id, model_id, body_type_id, color_id, fuel_type_id, transmission_type_id, drivetrain_id, doors_id, seats_id, production_year, mileage, engine_power, engine_capacity, is_damaged)
 VALUES
-    ('PQR56789012345678', 3, 5, 16, 6, 2, 1, 2, 1, 3, 4, 2021, 35000, 30000, 200, 2500, FALSE, CURRENT_TIMESTAMP, NULL)
+    ('ABC12345678901236', 10, 28, 8, 2, 1, 2, 1, 4, 5, 2013, 180000, 200, 1996, FALSE)
     RETURNING car_id
     ),
 
--- Insert car features
-    inserted_features AS (
-INSERT INTO car_features_junction (car_id, feature_id)
-SELECT car_id, feature_id
-FROM inserted_cars
-    CROSS JOIN (VALUES (1), (2), (3)) AS features(feature_id)
-    ),
-
--- Insert images into car_image_urls table and associate them with the car
     inserted_images AS (
 INSERT INTO car_image_urls (image_url)
 VALUES
-    ('https://iili.io/Ja9nYec.jpg'),
-    ('https://iili.io/Jad72sd.jpg')
+    ('https://iili.io/Ja9nYcs.jpg'),
+    ('https://iili.io/Jad72tv.jpg')
     RETURNING image_id
-    )
+    ),
 
-INSERT INTO car_image_urls_junction (car_id, image_id)
-SELECT ic.car_id, ii.image_id
-FROM inserted_cars ic
-         CROSS JOIN inserted_images ii
-WHERE ii.image_id IN (SELECT image_id FROM inserted_images);
+    image_juction AS (INSERT INTO car_image_urls_junction (car_id, image_id)
+SELECT inserted_car_id.car_id, inserted_images.image_id
+FROM inserted_car_id, inserted_images)
+
+INSERT INTO car_features_junction (car_id, feature_id)
+SELECT inserted_car_id.car_id, features.feature_id
+FROM inserted_car_id, (VALUES (1), (2), (5), (6), (7)) AS features(feature_id);
 
 
 -- Fourth car
--- Insert the fourth car and get its ID
-WITH inserted_cars AS (
-INSERT INTO cars (vin, seller_id, make_id, model_id, body_type_id, color_id, fuel_type_id, transmission_type_id, drivetrain_id, doors_id, seats_id, production_year, price, mileage, engine_power, engine_capacity, is_damaged, created_at, updated_at)
+WITH inserted_car_id AS (
+INSERT INTO cars (vin, make_id, model_id, body_type_id, color_id, fuel_type_id, transmission_type_id, drivetrain_id, doors_id, seats_id, production_year, mileage, engine_power, engine_capacity, is_damaged)
 VALUES
-    ('LMN45678901234567', 3, 7, 20, 7, 4, 2, 1, 2, 5, 7, 2020, 28000, 45000, 160, 1800, FALSE, CURRENT_TIMESTAMP, NULL)
+    ('ABC12345678901237', 4, 12, 8, 3, 4, 1, 2, 4, 5, 2011, 150000, 167, 1996, FALSE)
     RETURNING car_id
     ),
 
--- Insert car features
-    inserted_features AS (
-INSERT INTO car_features_junction (car_id, feature_id)
-SELECT car_id, feature_id
-FROM inserted_cars
-    CROSS JOIN (VALUES (1), (2), (3)) AS features(feature_id)
-    ),
-
--- Insert images into car_image_urls table and associate them with the car
     inserted_images AS (
 INSERT INTO car_image_urls (image_url)
 VALUES
-    ('https://iili.io/Ja9nYee.jpg'),
-    ('https://iili.io/Jad72sf.jpg')
+    ('https://iili.io/Ja9nYis.jpg'),
+    ('https://iili.io/Jad72qv.jpg')
     RETURNING image_id
+    ),
+
+    image_juction AS (INSERT INTO car_image_urls_junction (car_id, image_id)
+SELECT inserted_car_id.car_id, inserted_images.image_id
+FROM inserted_car_id, inserted_images)
+
+INSERT INTO car_features_junction (car_id, feature_id)
+SELECT inserted_car_id.car_id, features.feature_id
+FROM inserted_car_id, (VALUES (1), (2), (3), (5), (7)) AS features(feature_id);
+
+
+-- Advert attributes
+INSERT INTO regions (region_name)
+VALUES ('Zachodniopomorskie'),
+       ('Pomorskie'),
+       ('Wielkopolskie');
+
+INSERT INTO locations (region_id, postal_code, city)
+VALUES (1,'73-110', 'Stargard'),
+       (2, '80-180', 'Gdańsk'),
+       (3, '60-003', 'Poznań');
+
+
+-- Adverts
+
+-- First advert
+WITH inserted_advert AS (
+INSERT INTO adverts (seller_id, car_id, title, description, location_id, price)
+VALUES
+    (1, 1, 'Volkswagen Passat', 'Really nice Passat. Buy it pls.', 1, 99000)
+    RETURNING advert_id
+    ),
+
+    inserted_phone_numbers AS (
+INSERT INTO advert_phone_numbers (phone_number)
+VALUES
+    ('123456789'),
+    ('987654321')
+    RETURNING phone_number_id
     )
 
-INSERT INTO car_image_urls_junction (car_id, image_id)
-SELECT ic.car_id, ii.image_id
-FROM inserted_cars ic
-         CROSS JOIN inserted_images ii
-WHERE ii.image_id IN (SELECT image_id FROM inserted_images);
+INSERT INTO advert_phone_numbers_junction (advert_id, advert_phone_numbers)
+SELECT inserted_advert.advert_id, inserted_phone_numbers.phone_number_id
+FROM inserted_advert, inserted_phone_numbers;
+
+
+-- Second advert
+WITH inserted_advert AS (
+INSERT INTO adverts (seller_id, car_id, title, description, location_id, price)
+VALUES
+    (3, 2, 'Skoda Octavia', 'Nice Skodunia.', 2, 35000)
+    RETURNING advert_id
+    ),
+
+    inserted_phone_numbers AS (
+INSERT INTO advert_phone_numbers (phone_number)
+VALUES
+    ('123123123')
+    RETURNING phone_number_id
+    )
+
+INSERT INTO advert_phone_numbers_junction (advert_id, advert_phone_numbers)
+SELECT inserted_advert.advert_id, inserted_phone_numbers.phone_number_id
+FROM inserted_advert, inserted_phone_numbers;
+
+
+-- Third advert
+WITH inserted_advert AS (
+INSERT INTO adverts (seller_id, car_id, title, description, location_id, price)
+VALUES
+    (2, 3, 'BMW 3 Series', 'BMW wruuuum', 3, 45000)
+    RETURNING advert_id
+    ),
+
+    inserted_phone_numbers AS (
+INSERT INTO advert_phone_numbers (phone_number)
+VALUES
+    ('321321321')
+    RETURNING phone_number_id
+    )
+
+INSERT INTO advert_phone_numbers_junction (advert_id, advert_phone_numbers)
+SELECT inserted_advert.advert_id, inserted_phone_numbers.phone_number_id
+FROM inserted_advert, inserted_phone_numbers;
+
+
+-- Fourth advert
+WITH inserted_advert AS (
+INSERT INTO adverts (seller_id, car_id, title, description, location_id, price)
+VALUES
+    (1, 4, 'Opel Insignia', 'Opel wow', 2, 43000)
+    RETURNING advert_id
+    ),
+
+    inserted_phone_numbers AS (
+INSERT INTO advert_phone_numbers (phone_number)
+VALUES
+    ('432101234')
+    RETURNING phone_number_id
+    )
+
+INSERT INTO advert_phone_numbers_junction (advert_id, advert_phone_numbers)
+SELECT inserted_advert.advert_id, inserted_phone_numbers.phone_number_id
+FROM inserted_advert, inserted_phone_numbers;
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,127 +1,167 @@
--- CREATE TABLE statements for every table
+-- Entire database schema
 
+
+-- Users table
 CREATE TABLE users
 (
-    id         SERIAL PRIMARY KEY,
-    username   VARCHAR(20)                            NOT NULL,
-    email      VARCHAR(100)                           NOT NULL,
+    user_id    BIGSERIAL PRIMARY KEY,
+    username   VARCHAR(20)                            NOT NULL UNIQUE,
+    email      VARCHAR(100)                           NOT NULL UNIQUE,
     password   VARCHAR(255)                           NOT NULL,
     created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at TIMESTAMP(0) DEFAULT NULL,
     is_enabled BOOLEAN      DEFAULT TRUE              NOT NULL
 );
 
--- Reference tables for the cars table
+
+-- Cars table
 
 CREATE TABLE car_makes
 (
-    make_id   SERIAL PRIMARY KEY,
+    make_id   BIGSERIAL PRIMARY KEY,
     make_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE car_models
 (
-    model_id   SERIAL PRIMARY KEY,
-    model_name VARCHAR(255)                                   NOT NULL,
-    make_id    INTEGER REFERENCES car_makes ON DELETE CASCADE NOT NULL,
-    UNIQUE (model_name, make_id)
+    model_id   BIGSERIAL PRIMARY KEY,
+    model_name VARCHAR(255)                          NOT NULL,
+    make_id    BIGINT REFERENCES car_makes (make_id) NOT NULL,
+    UNIQUE (model_name, make_id),
+    UNIQUE (model_id, make_id)
 );
 
 CREATE TABLE car_body_types
 (
-    body_type_id   SERIAL PRIMARY KEY,
+    body_type_id   BIGSERIAL PRIMARY KEY,
     body_type_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE car_colors
 (
-    color_id   SERIAL PRIMARY KEY,
+    color_id   BIGSERIAL PRIMARY KEY,
     color_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE car_fuel_types
 (
-    fuel_type_id   SERIAL PRIMARY KEY,
+    fuel_type_id   BIGSERIAL PRIMARY KEY,
     fuel_type_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE car_transmission_types
 (
-    transmission_type_id   SERIAL PRIMARY KEY,
+    transmission_type_id   BIGSERIAL PRIMARY KEY,
     transmission_type_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE car_drivetrain_types
 (
-    drivetrain_type_id   SERIAL PRIMARY KEY,
+    drivetrain_type_id   BIGSERIAL PRIMARY KEY,
     drivetrain_type_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE car_door_counts
 (
-    door_count_id SERIAL PRIMARY KEY,
-    door_count    INTEGER NOT NULL UNIQUE
+    door_count_id BIGSERIAL PRIMARY KEY,
+    door_count    BIGINT NOT NULL UNIQUE
 );
 
 CREATE TABLE car_seat_counts
 (
-    seat_count_id SERIAL PRIMARY KEY,
-    seat_count    INTEGER NOT NULL UNIQUE
+    seat_count_id BIGSERIAL PRIMARY KEY,
+    seat_count    BIGINT NOT NULL UNIQUE
 );
-
--- Main cars table
 
 CREATE TABLE cars
 (
-    car_id               SERIAL PRIMARY KEY,
-    vin                  VARCHAR(17)                                     NOT NULL UNIQUE,
-    seller_id            INTEGER REFERENCES users ON DELETE CASCADE      NOT NULL,
-    make_id              INTEGER REFERENCES car_makes ON DELETE CASCADE  NOT NULL,
-    model_id             INTEGER REFERENCES car_models ON DELETE CASCADE NOT NULL,
-    body_type_id         INTEGER REFERENCES car_body_types               NOT NULL,
-    color_id             INTEGER REFERENCES car_colors                   NOT NULL,
-    fuel_type_id         INTEGER REFERENCES car_fuel_types               NOT NULL,
-    transmission_type_id INTEGER REFERENCES car_transmission_types       NOT NULL,
-    drivetrain_id        INTEGER REFERENCES car_drivetrain_types         NOT NULL,
-    doors_id             INTEGER REFERENCES car_door_counts              NOT NULL,
-    seats_id             INTEGER REFERENCES car_seat_counts              NOT NULL,
-    production_year      INTEGER                                         NOT NULL,
-    price                INTEGER                                         NOT NULL,
-    mileage              INTEGER                                         NOT NULL,
-    engine_power         INTEGER                                         NOT NULL,
-    engine_capacity      INTEGER                                         NOT NULL,
-    is_damaged           BOOLEAN                                         NOT NULL,
-    created_at           TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP          NOT NULL,
-    updated_at           TIMESTAMP(0) DEFAULT NULL
+    car_id               BIGSERIAL PRIMARY KEY,
+    vin                  VARCHAR(17)                                                     NOT NULL UNIQUE,
+    make_id              BIGINT REFERENCES car_makes (make_id)                           NOT NULL,
+    model_id             BIGINT REFERENCES car_models (model_id)                         NOT NULL,
+    body_type_id         BIGINT REFERENCES car_body_types (body_type_id)                 NOT NULL,
+    color_id             BIGINT REFERENCES car_colors (color_id)                         NOT NULL,
+    fuel_type_id         BIGINT REFERENCES car_fuel_types (fuel_type_id)                 NOT NULL,
+    transmission_type_id BIGINT REFERENCES car_transmission_types (transmission_type_id) NOT NULL,
+    drivetrain_id        BIGINT REFERENCES car_drivetrain_types (drivetrain_type_id)     NOT NULL,
+    doors_id             BIGINT REFERENCES car_door_counts (door_count_id)               NOT NULL,
+    seats_id             BIGINT REFERENCES car_seat_counts (seat_count_id)               NOT NULL,
+    production_year      INTEGER                                                         NOT NULL,
+    mileage              INTEGER                                                         NOT NULL,
+    engine_power         INTEGER                                                         NOT NULL,
+    engine_capacity      INTEGER                                                         NOT NULL,
+    is_damaged           BOOLEAN                                                         NOT NULL,
+    CONSTRAINT fk_make_model
+        FOREIGN KEY (make_id, model_id)
+            REFERENCES car_models (make_id, model_id)
 );
-
--- Tables that have their junction tables for the cars table
 
 CREATE TABLE car_features
 (
-    feature_id   SERIAL PRIMARY KEY,
+    feature_id   BIGSERIAL PRIMARY KEY,
     feature_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 CREATE TABLE car_image_urls
 (
-    image_id   SERIAL PRIMARY KEY,
+    image_id   BIGSERIAL PRIMARY KEY,
     image_url  VARCHAR(255)                           NOT NULL UNIQUE,
     created_at TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
--- Junction tables for the cars table
-
 CREATE TABLE car_features_junction
 (
-    car_id     INTEGER REFERENCES cars ON DELETE CASCADE         NOT NULL,
-    feature_id INTEGER REFERENCES car_features ON DELETE CASCADE NOT NULL,
+    car_id     BIGINT REFERENCES cars (car_id) ON DELETE CASCADE NOT NULL,
+    feature_id BIGINT REFERENCES car_features (feature_id)       NOT NULL,
     PRIMARY KEY (car_id, feature_id)
 );
 
 CREATE TABLE car_image_urls_junction
 (
-    car_id   INTEGER REFERENCES cars ON DELETE CASCADE           NOT NULL,
-    image_id INTEGER REFERENCES car_image_urls ON DELETE CASCADE NOT NULL,
+    car_id   BIGINT REFERENCES cars (car_id) ON DELETE CASCADE NOT NULL,
+    image_id BIGINT REFERENCES car_image_urls (image_id)       NOT NULL,
     PRIMARY KEY (car_id, image_id)
+);
+
+
+-- Adverts table
+
+CREATE TABLE regions
+(
+    region_id   BIGSERIAL PRIMARY KEY,
+    region_name VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE locations
+(
+    location_id BIGSERIAL PRIMARY KEY,
+    region_id   BIGINT REFERENCES regions (region_id) NOT NULL,
+    postal_code CHAR(6)                               NOT NULL,
+    city        VARCHAR(100)                          NOT NULL
+);
+
+CREATE TABLE advert_phone_numbers
+(
+    phone_number_id BIGSERIAL PRIMARY KEY,
+    phone_number    CHAR(9)
+);
+
+CREATE TABLE adverts
+(
+    advert_id   BIGSERIAL PRIMARY KEY,
+    seller_id   BIGINT REFERENCES users (user_id)                 NOT NULL,
+    car_id      BIGINT REFERENCES cars (car_id) ON DELETE CASCADE NOT NULL UNIQUE,
+    title       VARCHAR(100)                                      NOT NULL,
+    description TEXT                                              NOT NULL,
+    location_id BIGINT REFERENCES locations (location_id)         NOT NULL,
+    price       INTEGER                                           NOT NULL,
+    created_at  TIMESTAMP(0) DEFAULT CURRENT_TIMESTAMP            NOT NULL,
+    updated_at  TIMESTAMP(0) DEFAULT NULL
+);
+
+CREATE TABLE advert_phone_numbers_junction
+(
+    advert_id            BIGINT REFERENCES adverts (advert_id) ON DELETE CASCADE  NOT NULL,
+    advert_phone_numbers BIGINT REFERENCES advert_phone_numbers (phone_number_id) NOT NULL,
+    PRIMARY KEY (advert_id, advert_phone_numbers)
 );
